3046001e037e258f77aca7d9b34e77aef8735db4
dhruv3 <dhruv.mnit@gmail.com>
2018-06-29 21:01:40 -0400

    Using normal refs to get component value

--- react-app/src/Refs.js
+++ react-app/src/Refs.js
@@ -7,18 +7,28 @@ class RefsApp extends React.Component{
       txt: "No Intro"
     }
   }
+  //Issue 1:
+  // a: e.target.value,
+  // b: e.target.value
+  //Using the above convention led to updating a and b at same time.
+  //Fix 1: Use refs to distinguish input elements
+  //a: this.refs.a.value,
+  //b: this.refs.b.value
   update(e){
     this.setState({
-      a: e.target.value,
-      b: e.target.value
+      a: this.a.value,
+      b: this.refs.b.value
     })
   }
   render(){
+    //Ref actually returns the node that we're referencing
+    //We take that node and set equal to a. Then extract value from a 
     return(
       <div>
-        <input type="text" onChange = {this.update.bind(this)}/>{this.state.a}
+        <input ref={node => this.a = node}
+          type="text" onChange = {this.update.bind(this)}/>{this.state.a}
         <hr/>
-        <input type="text" onChange = {this.update.bind(this)}/>{this.state.b}
+        <input ref="b" type="text" onChange = {this.update.bind(this)}/>{this.state.b}
         <hr/>
       </div>
 
