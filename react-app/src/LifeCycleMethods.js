71c52de656656eb660feece2b55639eca3747fe4
dhruv3 <dhruv.mnit@gmail.com>
2018-07-03 17:59:34 -0400

    Adding content inside mount functions

--- react-app/src/LifeCycleMethods.js
+++ react-app/src/LifeCycleMethods.js
@@ -5,19 +5,38 @@ import ReactDOM from 'react-dom';
 class App extends React.Component{
   constructor(){
     super();
-
+    this.state = {val: 0};
+    this.update = this.update.bind(this)
+  }
+  //This is going to fire off right before component is mounted into the DOM, and it just lets us know that it's guaranteed to make it into the DOM.
+  componentWillMount(){
+    console.log('component will mount')
+  }
+  update(){
+    this.setState({val: (this.state.val+1)})
   }
   render(){
-
+    console.log('render')
+    return(
+      <button onClick={this.update}>{this.state.val}</button>
+    )
+  }
+  //This is going to fire off once our component has actually been mounted to the DOM.
+  componentDidMount(){
+    console.log('component did mount')
+  }
+  //This is going to fire off when our component is about to leave the DOM.
+  componentWillUnmount(){
+    console.log('component will unmount')
   }
 }
 
 class Wrapper extends React.Component{
   mount(){
-
+    ReactDOM.render(<App/>, document.getElementById('appElem'))
   }
-  mount(){
-
+  unmount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('appElem'))
   }
   render(){
     return(
@@ -29,3 +48,5 @@ class Wrapper extends React.Component{
     )
   }
 }
+
+export {Wrapper}
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {ClassApp} from './App';
 import {RefsApp} from './Refs';
+import {Wrapper} from './LifeCycleMethods';
 
 const Elem = () => (
   <div>
+    <h1>Use of Life Cycle Methods</h1>
+    <Wrapper/>
     <h1>Use React ref to Get a Reference to Specific Components</h1>
     <RefsApp/>
     <h1>State Component and Constructor</h1>
