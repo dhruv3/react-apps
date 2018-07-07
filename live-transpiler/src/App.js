0d4bb2685bd178c1e4de4c1f16d06e288bb6891a
dhruv3 <dhruv.mnit@gmail.com>
2018-07-07 14:21:04 -0400

    Setting up render and constructor

--- live-transpiler/src/App.js
+++ live-transpiler/src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: "/* add your jsx here */",
+      output: "",
+      err:""
+    }
+  }
+
+  update(){
+    this.setState({})
+  }
+
   render() {
     return (
       <div>
+        <header>{this.state.err}</header>
+        <div class="container">
+          <textarea
+            onChange={this.update.bind(this)}
+            defaultValue={this.state.input}
+            />
+          <pre>{this.state.output}</pre>
+        </div>
       </div>
     );
   }
