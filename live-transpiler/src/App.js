d0ba1ea83f59e50ec8aec76f3f3217c64abfa6a9
dhruv3 <dhruv.mnit@gmail.com>
2018-07-07 17:15:11 -0400

    Adding complete implementation

--- live-transpiler/src/App.js
+++ live-transpiler/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   constructor(){
@@ -10,15 +11,24 @@ class App extends Component {
     }
   }
 
-  update(){
-    this.setState({})
+  update(e){
+    let code = e.target.value;
+    try{
+      this.setState({
+        output: window.Babel.transform(code, {presets: ['es2015', 'react']}).code,
+        err: ''
+      })
+    }
+    catch(err){
+      this.setState({err: err.message})
+    }
   }
 
   render() {
     return (
       <div>
         <header>{this.state.err}</header>
-        <div class="container">
+        <div className="container">
           <textarea
             onChange={this.update.bind(this)}
             defaultValue={this.state.input}
