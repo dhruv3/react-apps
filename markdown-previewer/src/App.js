ccc66946a1aba7ee7364b79d2733a83af99d471c
dhruv3 <dhruv.mnit@gmail.com>
2018-07-09 17:16:39 -0400

    Setting CSS and input for this project

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: "/* add your markdown here */",
+      output: "",
+      err:""
+    }
+  }
+
+  update(e){
+    let code = e.target.value;
+    try{
+      this.setState({
+        output: window.Babel.transform(code, {presets: ['es2015', 'react']}).code,
+        err: ''
+      })
+    }
+    catch(err){
+      this.setState({err: err.message})
+    }
+  }
+
   render() {
+    let inpStyle = {height: '100%'}
     return (
-      <div>
+      <div className="container-fluid">
+        <div className="container-fluid header">
+          <div className="row">
+            <div className="col-lg-6 text-center">Editor</div>
+            <div className="col-lg-6 text-center">Previewer</div>
+          </div>
+        </div>
+        <div className="container-fluid inputArea">
+          <div className="row" style={inpStyle}>
+            <textarea
+              className="col-lg-6"
+              onChange={this.update.bind(this)}
+              defaultValue={this.state.input}
+              />
+            <pre className="col-lg-6">{this.state.output}</pre>
+          </div>
+        </div>
       </div>
     );
   }
