2dea59d73e3e6fa9c75bb8b111829c81d695cc02
dhruv3 <dhruv.mnit@gmail.com>
2018-07-08 22:11:16 -0400

    Setting up components

--- rand-quote-generator/src/App.js
+++ rand-quote-generator/src/App.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 
+class Author extends Component{
+  render(){
+    return(
+      <div id="author">
+
+      </div>
+    )
+  }
+}
+
+class Text extends Component{
+  render(){
+    return(
+      <div id="text">
+
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div id="quote-box">
+        <Text/>
+        <Author/>
+        <div className="buttons">
+          <span id="tweet-quote">
+            <a href="twitter.com/intent/tweet">Tweet</a>
+          </span>
+          <button id="new-quote">New Quote</button>
+        </div>
       </div>
     );
   }
