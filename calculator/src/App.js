eedb8517d27ceecbb3bbf2d216e9c0d7b896ea9a
dhruv3 <dhruv.mnit@gmail.com>
2018-07-19 21:20:12 -0400

    Adding ids and updating styling

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -6,37 +6,37 @@ class ButtonPad extends Component{
     return(
       <React.Fragment>
         <div className="row customBtn">
-          <button className="col-sm-6">AC</button>
-          <button className="col-sm-3">/</button>
-          <button className="col-sm-3">X</button>
+          <button id="clear" className="col-sm-6">AC</button>
+          <button id="divide" className="col-sm-3">/</button>
+          <button id="multiply" className="col-sm-3">X</button>
         </div>
         <div className="row customBtn">
-          <button className="col-sm-3">7</button>
-          <button className="col-sm-3">8</button>
-          <button className="col-sm-3">9</button>
-          <button className="col-sm-3">-</button>
+          <button id="seven" className="col-sm-3">7</button>
+          <button id="eight" className="col-sm-3">8</button>
+          <button id="nine" className="col-sm-3">9</button>
+          <button id="subtract" className="col-sm-3">-</button>
         </div>
         <div className="row customBtn">
-          <button className="col-sm-3">4</button>
-          <button className="col-sm-3">5</button>
-          <button className="col-sm-3">6</button>
-          <button className="col-sm-3">+</button>
+          <button id="four" className="col-sm-3">4</button>
+          <button id="five" className="col-sm-3">5</button>
+          <button id="six" className="col-sm-3">6</button>
+          <button id="add" className="col-sm-3">+</button>
         </div>
         <div className="row">
           <div className="col-sm-9">
               <div className="row customBtn">
-                  <button className="col-sm-4">1</button>
-                  <button className="col-sm-4">2</button>
-                  <button className="col-sm-4">3</button>
+                  <button id="one" className="col-sm-4">1</button>
+                  <button id="two" className="col-sm-4">2</button>
+                  <button id="three" className="col-sm-4">3</button>
               </div>
               <div className="row customBtn">
-                  <button className="col-md-8">0</button>
+                  <button id="zero" className="col-md-8">0</button>
                   <button className="col-md-4">.</button>
               </div>
           </div>
           <div className="col-md-3 customBtn equalContainer">
             <div className="row">
-                <button id="equal" className="col-md-12">=</button>
+                <button id="equals" className="col-md-12">=</button>
             </div>
           </div>
         </div>
