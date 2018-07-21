113dccb210376378376dc74fccc300a5c2e32f14
dhruv3 <dhruv.mnit@gmail.com>
2018-07-21 10:06:32 -0400

    Adding content to clock setting comp

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -3,14 +3,27 @@ import React, { Component } from 'react';
 class ClkSetting extends Component{
   render(){
     return(
-      <div>
-
-      </div>
+      <React.Fragment>
+        <div className="row">
+          <div className="col-sm-12" id="break-label">{this.props.header}</div>
+        </div>
+        <div className="row">
+          <button id= {`${this.props.name}-decrement`} class="offset-sm-2 col-sm-2">
+            <i class="fa fa-arrow-down fa-2x"></i>
+          </button>
+          <div className="col-sm-4" id="break-label">
+            {this.props.header}
+          </div>
+          <button id={`${this.props.name}-increment`} class="offset-sm-2 col-sm-2">
+            <i class="fa fa-arrow-up fa-2x"></i>
+          </button>
+        </div>
+      </React.Fragment>
     )
   }
 }
 
-class ClkCounter extends Component{
+class ClkDisplay extends Component{
   render(){
     return(
       <div className="row">
@@ -37,19 +50,32 @@ class ClkControl extends Component{
 }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      breakVal: 5,
+      sessionVal: 25
+    }
+  }
   render() {
     return (
       <div className="container" id="clkWrapper">
         <div className="row clkHeader">Pomodoro Clock</div>
         <div className="row">
           <div className="col-sm-6">
-            <ClkSetting/>
+            <ClkSetting
+              name = "break"
+              header = "Break Length"
+              defaultVal = {this.state.breakVal}/>
           </div>
           <div className="col-sm-6">
-            <ClkSetting/>
+            <ClkSetting
+              name = "session"
+              header = "Session Length"
+              defaultVal = {this.state.sessionVal}/>
           </div>
         </div>
-        <ClkCounter/>
+        <ClkDisplay/>
         <ClkControl/>
       </div>
     );
