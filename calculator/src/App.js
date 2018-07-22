86c6a0b507c8ee3c254a08621e47b85292dbe965
dhruv3 <dhruv.mnit@gmail.com>
2018-07-22 13:36:30 -0400

    Passing event handlers to child comp and adding values to html element

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -2,41 +2,45 @@ import React, { Component } from 'react';
 
 
 class ButtonPad extends Component{
+  constructor(){
+    super();
+  }
+
   render(){
     return(
       <React.Fragment>
         <div className="row customBtn">
-          <button id="clear" className="col-sm-6">AC</button>
-          <button id="divide" className="col-sm-3">/</button>
-          <button id="multiply" className="col-sm-3">X</button>
+          <button id="clear" className="col-sm-6" onClick={this.props.clearClick}>AC</button>
+          <button id="divide" className="col-sm-3" onClick={this.props.operatorClick}>/</button>
+          <button id="multiply" className="col-sm-3" onClick={this.props.operatorClick}>X</button>
         </div>
         <div className="row customBtn">
-          <button id="seven" className="col-sm-3">7</button>
-          <button id="eight" className="col-sm-3">8</button>
-          <button id="nine" className="col-sm-3">9</button>
-          <button id="subtract" className="col-sm-3">-</button>
+          <button id="seven" className="col-sm-3" value="7" onClick={this.props.numberClick}>7</button>
+          <button id="eight" className="col-sm-3" value="8" onClick={this.props.numberClick}>8</button>
+          <button id="nine" className="col-sm-3" value="9" onClick={this.props.numberClick}>9</button>
+          <button id="subtract" className="col-sm-3" onClick={this.props.operatorClick}>-</button>
         </div>
         <div className="row customBtn">
-          <button id="four" className="col-sm-3">4</button>
-          <button id="five" className="col-sm-3">5</button>
-          <button id="six" className="col-sm-3">6</button>
-          <button id="add" className="col-sm-3">+</button>
+          <button id="four" className="col-sm-3" value="4" onClick={this.props.numberClick}>4</button>
+          <button id="five" className="col-sm-3" value="5" onClick={this.props.numberClick}>5</button>
+          <button id="six" className="col-sm-3" value="6" onClick={this.props.numberClick}>6</button>
+          <button id="add" className="col-sm-3" onClick={this.props.operatorClick}>+</button>
         </div>
         <div className="row">
           <div className="col-sm-9">
               <div className="row customBtn">
-                  <button id="one" className="col-sm-4">1</button>
-                  <button id="two" className="col-sm-4">2</button>
-                  <button id="three" className="col-sm-4">3</button>
+                  <button id="one" className="col-sm-4" value="1" onClick={this.props.numberClick}>1</button>
+                  <button id="two" className="col-sm-4" value="2" onClick={this.props.numberClick}>2</button>
+                  <button id="three" className="col-sm-4" value="3" onClick={this.props.numberClick}>3</button>
               </div>
               <div className="row customBtn">
-                  <button id="zero" className="col-sm-8">0</button>
-                  <button className="col-sm-4">.</button>
+                  <button id="zero" className="col-sm-8" value="0" onClick={this.props.numberClick}>0</button>
+                  <button className="col-sm-4" onClick={this.props.decimalClick}>.</button>
               </div>
           </div>
           <div className="col-sm-3 customBtn equalContainer">
             <div className="row">
-                <button id="equals" className="col-sm-12">=</button>
+                <button id="equals" className="col-sm-12" onClick={this.props.handleEqualClick}>=</button>
             </div>
           </div>
         </div>
@@ -47,18 +51,40 @@ class ButtonPad extends Component{
 
 class App extends Component {
   constructor(){
-    super()
-    this.handleParentClick = this.handleParentClick.bind(this)
+    super();
+    this.handleNumberClick = this.handleNumberClick.bind(this)
+    this.handleDecimalClick = this.handleDecimalClick.bind(this)
+    this.handleOperatorClick = this.handleOperatorClick.bind(this)
+    this.handleClearClick = this.handleClearClick.bind(this)
+    this.handleEqualClick = this.handleEqualClick.bind(this)
   }
-  handleParentClick(e){
+  handleNumberClick(e){
+    debugger;
+  }
+  handleDecimalClick(e){
+
+  }
+  handleOperatorClick(e){
+
+  }
+  handleClearClick(e){
+
+  }
+  handleEqualClick(e){
 
   }
   render() {
     return (
-      <div className="container" id="calcMachine" onClick={this.handleParentClick}>
+      <div className="container" id="calcMachine">
         <FormulaScreen/>
         <DisplayScreen/>
-        <ButtonPad/>
+        <ButtonPad
+          numberClick = {this.handleNumberClick}
+          decimalClick = {this.handleDecimalClick}
+          operatorClick = {this.handleOperatorClick}
+          clearClick = {this.handleClearClick}
+          equalClick = {this.handleEqualClick}
+          />
       </div>
     );
   }
