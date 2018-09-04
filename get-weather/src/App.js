b35ab3e1a183539e7e0c14041e59108a7ed28472
dhruv3 <dhruv.mnit@gmail.com>
2018-09-04 00:43:16 -0400

    Working commit

--- get-weather/src/App.js
+++ get-weather/src/App.js
@@ -2,9 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: "",
+      type: "",
+      icon: ""
+    };
+  }
+
+  fetchWeather(api){
+    fetch(api)
+    .then(res => res.json())
+      .then(response => {
+        this.setState({
+          location: response.name,
+          type: response.weather[0]["main"],
+          icon: response.weather[0]["icon"]
+        })
+      })
+  }
+
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition((position) => {
+      const api = `https://fcc-weather-api.glitch.me/api/current?lat=${position.coords.latitude}&lon=${position.coords.longitude}`;
+      this.fetchWeather(api);
+    });
+  }
+
   render() {
     return (
-      <div>
+      <div className="container">
+        <h1>{this.state.location}</h1>
+        <div><img src={this.state.icon}/></div>
+        <p>{this.state.type}</p>
       </div>
     );
   }
