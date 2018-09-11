63e4e8dbfd1dce43b9fe2c0a42e0653b1159d14b
dhruv3 <dhruv.mnit@gmail.com>
2018-09-10 22:56:16 -0400

    Adding initial container files

---
+++ simon-game/src/Controller.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Controller extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Controller;
