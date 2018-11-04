67a4c041b9e75652649f2a8d6d93105eb7d5b4d6
dhruv3 <dhruv.mnit@gmail.com>
2018-11-04 14:14:49 -0500

    Fetching content and setting up components

--- stackline-mockup/src/App.js
+++ stackline-mockup/src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import Sidebar from './Sidebar.js';
+import MainBody from './MainBody.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: {},
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://api.jsonbin.io/b/5bdf3a85ced6254f0e79e6d9";
+    fetch(url)
+      .then(res => res.json())
+        .then(response => {
+          this.setState({
+            content: response[0]
+          })
+        })
+  }
   render() {
     return (
       <div>
+        <Sidebar content={this.state.content}/>
+        <MainBody content={this.state.content}/>
       </div>
     );
   }
