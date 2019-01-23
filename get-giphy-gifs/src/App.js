db92248adfef537c84a09747fdc7de44b7cd61ba
dhruv3 <dhruv.mnit@gmail.com>
2019-01-22 22:26:18 -0500

    Adding new file and setting up App structure

--- get-giphy-gifs/src/App.js
+++ get-giphy-gifs/src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import DisplayContainer from './displayContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div>Trending GIFs</div>
+        <div>
+          <button>Load GIFs</button>
+          <button>Randomize Order</button>
+          <button>Select One at Random</button>
+          <button>Clear ALL</button>
+        </div>
+        <DisplayContainer/>
       </div>
     );
   }
---
+++ get-giphy-gifs/src/displayContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class DisplayContainer extends Component {
+  render() {
+    return (
+      <div className="displayContainer">
+      </div>
+    );
+  }
+}
+
+export default DisplayContainer;
