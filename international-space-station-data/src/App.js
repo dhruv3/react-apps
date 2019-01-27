ed80ab72a7f696aec166d9b1a96fafbee7926704
dhruv3 <dhruv.mnit@gmail.com>
2019-01-26 19:36:44 -0500

    One component completed

--- international-space-station-data/src/App.js
+++ international-space-station-data/src/App.js
@@ -3,10 +3,42 @@ import CurrentSpaceCowboys from './CurrentSpaceCowboys';
 import ISSData from './ISSData';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      intervalId: 0,
+      cowboyContent: {},
+    }
+    this.timer = this.timer.bind(this)
+  }
+
+  //https://stackoverflow.com/questions/36299174/setinterval-in-a-react-app
+  componentDidMount() {
+    var intervalId = setInterval(this.timer, 5000);
+    // store intervalId in the state so it can be accessed later:
+    this.setState({intervalId: intervalId});
+  }
+  
+  componentWillUnmount() {
+    // use intervalId from the state to clear the interval
+    clearInterval(this.state.intervalId);
+  }
+  
+  timer() {
+    var that = this;
+    fetch("https://cors-anywhere.herokuapp.com/http://api.open-notify.org/astros.json")
+      .then(res => res.json())
+        .then(function(items){
+                that.setState({cowboyContent: items})
+              }
+            )
+  }
+
   render() {
     return (
       <div className="container">
-        <CurrentSpaceCowboys/>
+        <h1>International Space Station Information</h1>
+        <CurrentSpaceCowboys content={this.state.cowboyContent}/>
         <ISSData/>
       </div>
     );
--- international-space-station-data/src/CurrentSpaceCowboys.js
+++ international-space-station-data/src/CurrentSpaceCowboys.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
 class CurrentSpaceCowboys extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      content: {}
+    };
+  }
+
+  componentWillReceiveProps(props){
+    if(props.content != undefined)
+      this.setState({content: props.content})
+  }
+
   render() {
     return (
       <div className="currentSpaceCowboysContainer">
+        {<h3>At this moment there are {this.state.content.number != undefined ? this.state.content.number : 0} people in space. They are:</h3>}
+        {
+          this.state.content.people != undefined ? this.state.content.people.map((elem, index) => {
+            return (
+              <div key={index} className="listItem">
+                {elem.name}, craft: {elem.craft}
+              </div>
+            )
+          }) : "" 
+        }
       </div>
     );
   }
