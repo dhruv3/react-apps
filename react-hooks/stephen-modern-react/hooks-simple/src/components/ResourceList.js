40706be51b096a494121e338184ef54abf48f868
dhruv3 <dhruv.mnit@gmail.com>
2020-04-21 15:30:16 -0500

    working with useEffect

---
+++ react-hooks/stephen-modern-react/hooks-simple/src/components/ResourceList.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const ResourceList = ({resource}) => {
+    const [resources, setResources] = useState([]);
+
+    const fetchResource = async (resource) => {
+        const response = await axios.get(
+            `https://jsonplaceholder.typicode.com/${resource}`
+        );
+
+        setResources(response.data);
+        //this.setState({resources: response.data});
+    }
+
+    useEffect(() => {
+        fetchResource(resource)
+    }, []);
+
+    return <div>{resources.length}</div>
+}
+
+export default ResourceList;
