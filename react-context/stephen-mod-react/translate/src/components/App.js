d1179700dc098f9eef6354acfeab4a2259b40c6d
dhruv3 <dhruv.mnit@gmail.com>
2020-05-03 23:54:33 -0500

    Setting up content

--- react-context/stephen-mod-react/translate/src/components/App.js
+++ react-context/stephen-mod-react/translate/src/components/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 
 class App extends React.Component{
+    state = {language: 'english'};
+
+    onLangChange = (language) => {
+        this.setState({language});
+    }
+
     render(){
         return(
-            <div className="ui container">App</div>
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i className="flag us" onClick={() => this.onLangChange('english')}/>
+                    <i className="flag nl" onClick={() => this.onLangChange('dutch')}/>
+                </div>
+                <UserCreate/>
+            </div>
         )
     }
 }
